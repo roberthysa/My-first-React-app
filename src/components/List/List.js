6c600279d732eda933fe4c1aa31d26323c7451e2
Robert Hysa <robert.hysa@wp.pl>
2021-04-07 18:08:11 +0200

    add data from dataStore.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,15 +3,18 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
     image: PropTypes.string,
+    columns: PropTypes.array,
   }
   static defaultProps = {
-  children: <p>I can do all the things!!!</p>,
+  description: settings.defaultListDescription,
   }
 
   render() {
@@ -19,7 +22,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageUrl={this.props.image} />
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column title='Animals' />
