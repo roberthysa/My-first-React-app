0fa0e44752e2e99034d79f7ae27a2dca2e484206
Robert Hysa <robert.hysa@wp.pl>
2021-04-07 11:13:17 +0200

    add mentor sugestion to component Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from './Hero.scss';
 const Hero = () => (
   <header className={styles.component}>
     <h2 className={styles.title}>Things to do</h2>
-    <img className={styles.image} src={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}/>
+    <img className={styles.image} src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'/>
   </header>
 );
 
